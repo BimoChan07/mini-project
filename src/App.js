d374c7072006c4b4871aaa4da90f6df856752311
BimoChan07 <bmochanb@gmail.com>
2023-07-17 16:48:57 +0545

    Greeting app created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 function App(){
+let recentDate = new Date(2023,7,17,11);
+recentDate = recentDate.getHours();
+let greeting = "";
+const cssStyle = {};
+
+if(recentDate>1 && recentDate<12){
+  greeting = "Good Morning";
+  cssStyle.color = "#0065AF";
+}else if(recentDate>=12 && recentDate<20){
+  greeting = "Good Afternoon";
+    cssStyle.color = "orange";
+}else if(recentDate>=20 && recentDate<=24){
+  greeting = "Good Evening";
+    cssStyle.color = "black";
+}
 
 };
 
