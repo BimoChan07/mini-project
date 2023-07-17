4af9b70c1b590fe5b91285c0417d79ae96139856
BimoChan07 <bmochanb@gmail.com>
2023-07-17 17:07:31 +0545

    simple app developed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 
 function App(){
-let recentDate = new Date(2023,7,17,11);
+let recentDate = new Date();
 recentDate = recentDate.getHours();
 let greeting = "";
 const cssStyle = {};
 
 if(recentDate>1 && recentDate<12){
   greeting = "Good Morning";
-  cssStyle.color = "#0065AF";
+  cssStyle.color = "";
 }else if(recentDate>=12 && recentDate<20){
   greeting = "Good Afternoon";
     cssStyle.color = "orange";
 }else if(recentDate>=20 && recentDate<=24){
- 
+  greeting = "Good Evening";
+    cssStyle.color = "black";
 }
 
+return(
+  <>
+  <div>
+  <h1>Hello Bimochan, <span style={cssStyle}>{greeting}</span></h1>
+  </div> 
+  </>
+  );
 };
 
 export default App;
